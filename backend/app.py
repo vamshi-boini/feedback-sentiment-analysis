b89d552d8c1209355b2a358a7220d177f04c776a
vamshi-boini <boini.vamshi01@gmail.com>
2026-01-09 02:05:38 +0530

    Add Vercel domain to CORS

--- backend/app.py
+++ backend/app.py
@@ -20,12 +20,13 @@ CORS(
         "origins": [
             "http://127.0.0.1:5173",
             "http://localhost:5173",
-            "https://*.vercel.app",  # Allow Vercel
-            "https://feedback-sentiment-analysis-nlld.onrender.com"
+            "https://feedback-sentiment-analysis-2rlr.vercel.app",  # Your Vercel domain
+            "https://*.vercel.app"  # Allow all Vercel preview deployments
         ]
     }},
     supports_credentials=True,
 )
+
 
 
 # Config
